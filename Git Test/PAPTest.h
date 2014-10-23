e07f1512771c6abfbf0ddcb0109ce9068bd98895
Pierre-Alexis Panel <pierrealexispanel@MacBook-Pro-de-Pierre-Alexis.local>
2014-10-23 18:04:58 +0200

    added test class

---
+++ Git Test/PAPTest.h
@@ -0,0 +1,13 @@
+//
+//  PAPTest.h
+//  Git Test
+//
+//  Created by Pierre-Alexis Panel on 23/10/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PAPTest : NSObject
+
+@end
---
+++ Git Test/PAPTest.m
@@ -0,0 +1,13 @@
+//
+//  PAPTest.m
+//  Git Test
+//
+//  Created by Pierre-Alexis Panel on 23/10/2014.
+//
+//
+
+#import "PAPTest.h"
+
+@implementation PAPTest
+
+@end
